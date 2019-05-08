12cf9d620ef5f260532313cddbed4d50c598c415
Keith Marble <kmarble96@gmail.com>
2019-05-08 12:17:06 -0400

    contact form update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => (
                                         <p className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></p>
                                         <p className="6u 12u$(xsmall)"><input type="email" name="email" id="_replyto" placeholder="Email" /></p>
                                         <p className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></p>
-                                        <p><button type="submit">Submit</button></p>
+                                        <p><button className="button" type="submit">Submit</button></p>
                                     </div>
                                 </form>
                             </div>
